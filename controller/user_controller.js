70a2329618f83c5294751e5b59d7610f140711c3
Karan <karan13699@gmail.com>
2023-02-05 13:57:01 +0530

    Template engine

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,6 +1,10 @@
 module.exports.profile = function(req, res){
-  return res.end('<h1> User Profile </h1>');
+  return res.render('profile',{
+    title:'profile'
+  });
 }
+
+
 
 
 module.exports.name = function(req, res){
